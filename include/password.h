b859352c680b7d53e4fb5d2ee40653c9bb010106
Pablo RDev <pabloriojod@gmail.com>
2025-06-05 20:16:22 -0400

    password.h

--- include/password.h
+++ include/password.h
@@ -1,8 +1,26 @@
 //
 // Created by Pablo RD on 5/6/25.
 //
-
+// RESPONSIBILITIES: AUTHENTICATION, LOADING PASSWORD, SAVING PASSWORD
 #ifndef PASSWORD_H
 #define PASSWORD_H
 
-#endif //PASSWORD_H
+#include <stdbool.h>
+
+typedef enum {
+    PASSWORD_OK,
+    PASSWORD_ERR_IO,
+    PASSWORD_ERR_HASH_MISMATCH,
+    PASSWORD_ERR_CRYPTO,
+} PasswordStatus;
+
+// Verifies the user's master password or creates a new one if no vault exists
+PasswordStatus authenticate_master_password(bool *is_new_password);
+
+// Loads the stored hash and salt from the vault metadata
+PasswordStatus load_password_data(unsigned char *hash_out, unsigned char *salt_out);
+
+// Saves a new hash and salt to the vault metadata
+PasswordStatus save_password_data(const unsigned char *hash, const unsigned char *salt);
+
+#endif // PASSWORD_H
